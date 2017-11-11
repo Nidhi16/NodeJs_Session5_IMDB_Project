0f149768c11e822acae6b75951f215c48b456665
Nidhi16 <nidhi.valuable@gmail.com>
2017-11-11 13:50:11 +0530

    Made theatre.js and added theatre model to it

---
+++ models/theatre.js
@@ -0,0 +1,44 @@
+var imdb = imdb || {};
+
+function Theatre(title, length, rating, country) {
+    this._id = ++Theatre.id;
+    this.title = title;
+    this.length = length;
+    this.rating = rating;
+    this.country = country;
+    this._actors = [];
+    this._reviews = [];
+
+    this.addActor = function(actorObject) {
+        this._actors.unshift(actorObject);
+    };
+    this.getActors = function(){
+        return this._actors;
+    };
+    this.addReview = function(reviewObject) {
+        this._reviews.unshift(reviewObject);
+    };
+    this.deleteReview = function(reviewObject) {
+        this._reviews.pop(reviewObject);
+    };
+    this.deleteReviewById = function(id) {
+        var review_to_remove;
+        this._reviews.forEach(function (review) {
+            if (review._id == id) {
+                review_to_remove = review;
+            }
+        }, this);
+        if (review_to_remove) {
+            this._reviews.pop(review_to_remove);
+        }
+    };
+    this.getReviews = function() {
+        return this._reviews;
+    };
+}
+Theatre.id = 0;
+
+imdb.getTheatre = function(title, length, rating, country) {
+    var theatre = new Theatre(title, length, rating, country);
+    return theatre;
+};
